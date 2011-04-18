7c34e05b82c8fff7994aa114a09da9536eb94f5d
Jay McCarthy <jay.mccarthy@gmail.com>
2011-04-18 16:22:18 -0600

    init

---
+++ games/maze/maze.rkt
@@ -0,0 +1,105 @@
+#lang racket/gui
+(require racket/runtime-path
+         "../../exp/loop.rkt"
+         (prefix-in gl: 
+                    (combine-in "../../exp/gl.rkt"
+                                "../../exp/gl-ext.rkt"))
+         "../../exp/sprites.rkt"
+         "../../exp/mvector.rkt"
+         "../../exp/fullscreen.rkt"
+         "../../exp/keyboard.rkt"
+         "../../exp/mapping.rkt"
+         "../../exp/controller.rkt"
+         "../../exp/joystick.rkt"
+         "../../exp/3s.rkt"
+         "../../exp/psn.rkt"
+         (prefix-in cd: "../../exp/cd-narrow.rkt"))
+
+(define-runtime-path resource-path "r")
+(define-syntax-rule (define-sound id f)
+  (define id (path->audio (build-path resource-path f))))
+(define-syntax-rule (define-texture id f)
+  (define id (gl:path->texture (build-path resource-path f))))
+             
+(define width 29)
+(define height 32)
+(define center-pos
+  (psn (/ width 2.) (/ height 2.)))
+
+(define layout
+  (vector  
+   1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+   1 0 0 1 1 1 0 0 1 1 1 1 0 0 1
+   1 0 0 1 1 1 0 0 1 1 1 1 0 0 1
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+   1 0 0 1 1 1 0 0 1 0 0 1 1 1 1
+   1 0 0 0 0 0 0 0 1 0 0 0 0 0 1
+   1 0 0 0 0 0 0 0 1 0 0 0 0 0 1
+   1 1 1 1 1 1 0 0 1 1 1 1 0 0 1
+   1 1 1 1 1 1 0 0 1 0 0 0 0 0 0
+   1 1 1 1 1 1 0 0 1 0 0 0 0 0 0
+   1 1 1 1 1 1 0 0 1 0 0 1 1 1 2 ; The gate
+   0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+   0 0 0 0 0 0 0 0 0 0 0 1 0 0 0
+   1 1 1 1 1 1 0 0 1 0 0 1 1 1 1
+   1 1 1 1 1 1 0 0 1 0 0 0 0 0 0
+   1 1 1 1 1 1 0 0 1 0 0 0 0 0 0
+   1 1 1 1 1 1 0 0 1 0 0 1 1 1 1
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 1
+   1 0 0 1 1 1 0 0 1 1 1 1 0 0 1
+   1 0 0 0 0 1 0 0 0 0 0 0 0 0 0
+   1 0 0 0 0 1 0 0 0 0 0 0 0 0 0
+   1 1 1 0 0 1 0 0 1 0 0 1 1 1 1
+   1 0 0 0 0 0 0 0 1 0 0 0 0 0 1
+   1 0 0 0 0 0 0 0 1 0 0 0 0 0 1
+   1 0 0 1 1 1 1 1 1 1 1 1 0 0 1
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+   1 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+   1 1 1 1 1 1 1 1 1 1 1 1 1 1 1))
+
+; XXX turn the layout into a nice graphic with rounded tiles, etc
+; XXX turn the layout into a set of collision shapes
+; XXX place the ghosts and pellets into the layout
+; XXX run around the map
+; XXX get points
+; XXX kill ghosts / be killed
+; XXX render ui
+
+#;(define-texture sprites-t "pacman.png")
+(define whole-map
+  (gl:color 0 0 255 0
+            (gl:for*/gl
+             ([c (in-range 29)]
+              [r (in-range 32)])
+             (define vc
+               (if (c . <= . 14)
+                   c
+                   
+                   (- 14 (- c 14))))
+             (gl:translate c (- height r 1)
+                           (if (= 1 (vector-ref layout (+ (* r 15) vc)))
+                               (gl:rectangle 1. 1.)
+                               gl:blank)))))
+
+(big-bang
+   0
+   #:sound-scale
+   (/ width 2.)
+   #:tick
+   (λ (w cs)
+     (values 
+      (add1 w)
+      (gl:focus 
+       width height width height
+       (psn-x center-pos) (psn-y center-pos)
+       (gl:background 0 0 0 0
+                      whole-map))
+      empty))
+   #:listener
+   (λ (w) center-pos)
+   #:done?
+   (λ (w) #f))
