b61378f5bab33d8d0c58153631c120251868931f
Jay McCarthy <jay.mccarthy@gmail.com>
2012-06-20 18:22:33 -0600

    Remove OSX joysticks

--- exp/joystick.rkt
+++ exp/joystick.rkt
@@ -1,6 +1,26 @@
 #lang racket/base
-(require "system-select.rkt")
+(require racket/list
+         racket/contract)
 
-(system-select
- [(unix) "joystick-unix.rkt"]
- [(macosx) "joystick-osx.rkt"])
+; A joystick-state is a
+;  sticks : stick -> (axis 0 / pov 1) -> which -> int
+;  buttons : button -> boolean
+(struct joystick-state (sticks buttons) #:transparent)
+
+(provide/contract 
+ [struct joystick-state
+         ([sticks 
+           (vector-immutableof
+            (vector/c
+             #:immutable #t
+             (vector-immutableof number?)
+             (vector-immutableof number?)))]
+          [buttons
+           (vector-immutableof boolean?)])])
+
+(define (get-all-joystick-snapshot-thunks)
+  empty)
+
+(provide/contract
+ [get-all-joystick-snapshot-thunks
+  (-> (listof (-> joystick-state?)))])
