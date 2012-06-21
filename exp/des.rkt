9510ff4bc49743c34ec5b96559977f0bc320050b
Jay McCarthy <jay.mccarthy@gmail.com>
2012-06-20 18:20:08 -0600

    generics

--- exp/des.rkt
+++ exp/des.rkt
@@ -2,37 +2,36 @@
 (require racket/match
          racket/list
          racket/function
-         unstable/generics)
+         racket/generic)
 
 (define-syntax-rule (define/for/fold ([id init] ...) . more)
   (define-values (id ...)
     (for/fold ([id init] ...) . more)))
 
 ;; Objects
-(generics reactable
-          (react reactable evts))
+(define-generics reactable
+  (react reactable evts))
 
 ;; Meshs
 (struct mesh (objs)
         #:transparent
-        #:property prop:reactable
-        (define-methods reactable
-          (define/generic gen-react react)
-          (define (react this events)
-            (match-define (mesh objs) this)
-            (define/for/fold ([new-objects (hasheq)]
-                              [new-events empty])
-              ([(oid obj) (in-hash objs)])
-              (define these-events
-                (append
-                 (filter (curry mesh-visible-to? oid) events)
-                 (mesh-collisions oid)))
-              (define-values (new-obj these-new-events)
-                (gen-react obj these-events))
-              (values (hash-set new-objects oid new-obj)
-                      (append these-new-events new-events)))
-            (values (mesh new-objects)
-                    new-events))))
+        #:methods gen:reactable
+        ((define/generic gen-react react)
+         (define (react this events)
+           (match-define (mesh objs) this)
+           (define/for/fold ([new-objects (hasheq)]
+                             [new-events empty])
+             ([(oid obj) (in-hash objs)])
+             (define these-events
+               (append
+                (filter (curry mesh-visible-to? oid) events)
+                (mesh-collisions oid)))
+             (define-values (new-obj these-new-events)
+               (gen-react obj these-events))
+             (values (hash-set new-objects oid new-obj)
+                     (append these-new-events new-events)))
+           (values (mesh new-objects)
+                   new-events))))
 
 (define (new-mesh)
   (mesh (hasheq)))
@@ -53,17 +52,16 @@
 
 (struct counter (n)
         #:transparent
-        #:property prop:reactable
-        (define-methods reactable
-          (define (react this events)
-            (match-define (counter n) this)
-            (match events
-              [(list)
-               (values this
-                       empty)]
-              [(list evt_1 ... (tick frames) evt_2 ...)
-               (values (counter (add1 n))
-                       empty)]))))
+        #:methods gen:reactable
+        ((define (react this events)
+           (match-define (counter n) this)
+           (match events
+             [(list)
+              (values this
+                      empty)]
+             [(list evt_1 ... (tick frames) evt_2 ...)
+              (values (counter (add1 n))
+                      empty)]))))
 
 (module+ test
   (require rackunit)
