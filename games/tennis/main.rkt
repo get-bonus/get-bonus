ed92fbdf2be2567956057e8f7ba739fc67c5f5fc
Jay McCarthy <jay.mccarthy@gmail.com>
2012-06-30 15:36:34 -0600

    First pass at OS model

--- games/tennis/main.rkt
+++ games/tennis/main.rkt
@@ -1,5 +1,9 @@
-#lang racket/gui
+#lang racket/base
 (require racket/runtime-path
+         racket/match
+         racket/function
+         racket/math
+         racket/list
          gb/gui/world
          (prefix-in gl:
                     (combine-in gb/graphics/gl
@@ -79,7 +83,7 @@
 (define ball-sprites
   (gl:path->texture
    (build-path resource-path "ryu.png")))
-(define ball
+(define gl:ball
   (gl:translate (- ball-r) (- ball-r)
                 (gl:texture/px ball-sprites
                                (* 2 ball-hw) (* 2 ball-hh)
@@ -144,202 +148,327 @@
   (and ((max lhs rhs) . >= . at-least)
        ((abs (- lhs rhs)) . >= . over)))
 
-(define server 'left)
+;; XXX Make the ball whoosh
+;; XXX Make scores have calls
+
 (define (game-start)
-  (big-bang
-   (game-st 0 #t
-            0 0
-            4.5
-            (start-pos 4.5 4.5 server)
-            (start-dir server) (opposite server)
-            4.5)
-   #:sound-scale
-   width-h
-   #:tick
-   (λ (w cs)
-     (match-define (game-st
-                    frame serving?
-                    lhs-score rhs-score
-                    lhs-y
-                    ball-pos ball-dir ball-tar
-                    rhs-y)
-                   w)
-     (match-define
-      (list (and
-             (app controller-a
-                  lhs-serve?)
-             (app controller-dpad
-                  (app psn-y
-                       lhs-dy)))
-            (and
-             (app controller-a
-                  rhs-serve?)
-             (app controller-dpad
-                  (app psn-y
-                       rhs-dy))))
-      (if (= (length cs) 2)
-        cs
-        (list
-         (first cs)
-         (controller
-          (psn 0.
-                                        ; Goes towards the ball's y position
-               (clamp -1.
-                      (/ (- (psn-y ball-pos) rhs-y) speed)
-                      1.))
-                                        ; Serves immediately
-          (if (and serving? (eq? server 'right))
-            #t
-            #f)
-          #f #f #f #f
-          #f #f #f #f #f #f))))
+  ;; OS library
+  (define kernel-prompt-tag (make-continuation-prompt-tag 'kernel))
+  (define (run-process-until-syscall p)
+    (call-with-continuation-barrier
+     (λ ()
+       (call-with-continuation-prompt
+        (λ ()
+          (p)
+          (error 'kernel "Process did not run to system call"))
+        kernel-prompt-tag
+        (λ (x) x)))))
+  (define (trap-syscall k->syscall)
+    ;; First we capture our context back to the OS
+    (call-with-current-continuation
+     (λ (k)
+       ;; Then we abort, give it to the OS, along with a syscall
+       ;; specification
+       (abort-current-continuation kernel-prompt-tag (k->syscall k)))
+     kernel-prompt-tag))
 
-     (define lhs-y-n
-       (clamp
-        min-paddle-y
-        (+ lhs-y (* lhs-dy speed))
-        max-paddle-y))
-     (define rhs-y-n
-       (clamp
-        min-paddle-y
-        (+ rhs-y (* rhs-dy speed))
-        max-paddle-y))
-     (define (ball-in-dir dir)
-       (+ ball-pos (make-polar ball-speed dir)))
-     (define ball-pos-m
-       (if serving?
-         (start-pos lhs-y-n rhs-y-n server)
-         (ball-in-dir ball-dir)))
+  (define-syntax-rule
+    (define-syscall (call k call-arg ...) state-args
+      body ...)
+    (define call
+      (let ()
+        (struct call (k call-arg ...)
+                #:property prop:procedure
+                (λ (the-struct . state-args)
+                  (match-define (call k call-arg ...) the-struct)
+                  body ...)
+                #:transparent)
+        (λ (call-arg ...)
+          (trap-syscall
+           (λ (k)
+             (call k call-arg ...)))))))
 
-     (define ball-shape
-       (cd:aabb ball-pos-m ball-hw ball-hh))
-     (define lhs-shape
-       (cd:aabb (psn (+ lhs-x paddle-hw) lhs-y-n)
-                paddle-hw paddle-hh))
-     (define rhs-shape
-       (cd:aabb (psn (+ rhs-x paddle-hw) rhs-y-n)
-                paddle-hw paddle-hh))
+  (define-syntax-rule
+    (define-syscalls state-args
+      [call-spec . body]
+      ...)
+    (begin
+      (define-syscall call-spec state-args
+        . body)
+      ...))
 
-     (define-values
-       (ball-pos-n+ ball-dir-n ball-tar-n serving?-p sounds)
-       (cond
-         [serving?
-          (cond
-            [(and (eq? server 'right) rhs-serve?)
-             (values ball-pos-m ball-dir ball-tar #f
-                     (list (sound-at se:bump-rhs ball-pos-m)))]
-            [(and (eq? server 'left) lhs-serve?)
-             (values ball-pos-m ball-dir ball-tar #f
-                     (list (sound-at se:bump-lhs ball-pos-m)))]
-            [else
-             (values ball-pos-m ball-dir ball-tar serving? empty)])]
-         [; The ball hit the top
-          (cd:shape-vs-shape ball-shape frame-top)
-          (values ball-pos
-                  (case ball-tar
-                    [(left) (between 3.2 4.2)]
-                    [(right) (between 5.2 6.2)])
-                  ball-tar #f
-                  (list (sound-at se:bump-wall ball-pos-m)))]
-         [; The ball hit the bot
-          (cd:shape-vs-shape ball-shape frame-bot)
-          (values ball-pos
-                  (case ball-tar
-                    [(left) (between 2.1 3.0)]
-                    [(right) (between 0.2 1.1)])
-                  ball-tar #f
-                  (list (sound-at se:bump-wall ball-pos-m)))]
-         [; The ball has bounced off the lhs
-          (cd:shape-vs-shape ball-shape lhs-shape)
-          (values ball-pos
-                  (random-dir 'right) 'right #f
-                  (list (sound-at se:bump-lhs ball-pos-m)))]
-         [; The ball has bounced off the rhs
-          (cd:shape-vs-shape ball-shape rhs-shape)
-          (values ball-pos
-                  (random-dir 'left) 'left #f
-                  (list (sound-at se:bump-rhs ball-pos-m)))]
-                                        ; The ball is inside the frame
-         [else
-          (values ball-pos-m ball-dir ball-tar #f empty)]))
-     (define ball-pos-n
-       (if (= ball-dir-n ball-dir)
-         ball-pos-n+
-         (ball-in-dir ball-dir-n)))
+  (define (snoc* beginning . end)
+    (append beginning end))
 
-     (define-values
-       (ball-pos-p ball-dir-p ball-tar-p
-                   serving?-n lhs-score-n rhs-score-n score?)
-       (cond
-                                        ; The ball has moved to the left of the lhs paddle
-         [((psn-x ball-pos-n) . < . lhs-x)
-          (values (start-pos lhs-y rhs-y server) (start-dir server)
-                  (opposite server) #t lhs-score (add1 rhs-score) #t)]
-                                        ; The ball has moved to the right of the rhs paddle
-         [((psn-x ball-pos-n) . > . rhs-x)
-          (values (start-pos lhs-y rhs-y server) (start-dir server)
-                  (opposite server) #t (add1 lhs-score) rhs-score #t)]
-         [else
-          (values ball-pos-n ball-dir-n ball-tar-n
-                  serving?-p lhs-score rhs-score #f)]))
+  (struct process (pid k) #:transparent)
+  (struct os (cur-heap next-heap cur-procs next-procs))
 
-     (values
-      (game-st
-       (add1 frame) serving?-n
-       lhs-score-n rhs-score-n
-       lhs-y-n
-       ball-pos-p ball-dir-p ball-tar-p
-       rhs-y-n)
-      (gl:focus
-       width height width height
-       (psn-x center-pos) (psn-y center-pos)
-       (gl:seqn
-        bgm
-        (let ()
-          (define score-t
-            (gl:string->texture
-             #:size 30
-             (format "(~a:~a)"
-                     lhs-score-n rhs-score-n)))
-          (gl:translate
-           (- (psn-x center-pos) (/ (gl:texture-dw score-t) 2))
-           (- height (gl:texture-dh score-t))
-           (gl:seqn
-            (gl:color 1. 1. 1. 0.
-                      (gl:rectangle (gl:texture-dw score-t)
-                                    (gl:texture-dh score-t)))
-            (gl:color 0. 0. 0. 1.
-                      (gl:texture score-t)))))
-        ;; XXX Add a collision animation
-        (gl:translate lhs-x (- lhs-y-n paddle-hh)
-                      lhs-paddle)
-        (gl:translate rhs-x (- rhs-y-n paddle-hh)
-                      rhs-paddle)
-        (gl:translate (psn-x ball-pos-p) (psn-y ball-pos-p)
-                      (gl:rotate (* (/ 180 pi) ball-dir-p)
-                                 ball))))
-      ;; XXX Make the ball whoosh
-      ;; XXX Make scores have calls
-      (append
-       sounds
-       (if score?
-         (list (sound-at se:applause center-pos))
+  (define-syscalls (pid current)
+    [(os/read k id)
+     (match-define (os cur-h next-h cur-ps next-ps) current)
+     (os cur-h next-h
+         (snoc* cur-ps
+                (process pid (λ () (k (hash-ref cur-h id empty)))))
+         next-ps)]
+    [(os/write k id*vals)
+     (match-define (os cur-h next-h cur-ps next-ps) current)
+     (define next-h-n
+       (for/fold ([next-h next-h])
+           ([id*val (in-list id*vals)])
+         (match-define (cons id val) id*val)
+         (hash-update next-h id (curry cons val) (λ () empty))))
+     (os cur-h next-h-n cur-ps
+         (snoc* next-ps
+                (process pid (λ () (k (void))))))]
+    [(os/thread k t)
+     (match-define (os cur-h next-h cur-ps next-ps) current)
+     (define t-pid (gensym 'pid))
+     (os cur-h next-h
+         (snoc* cur-ps
+                (process pid (λ () (k t-pid)))
+                (process t-pid t))
+         next-ps)])
+
+  (define (os/read* k [def #f])
+    (match (os/read k)
+      [(list v)
+       v]
+      [else
+       (if def
+         def
+         (error 'os/read* "~e does not have one value, has ~e" k else))]))
+
+  (define boot
+    (match-lambda
+     [(os cur-h next-h (list) next-ps)
+      (os next-h (hasheq) next-ps (list))]
+     [(os cur-h next-h (list* (process pid now) cur-ps) next-ps)
+      (define syscall (run-process-until-syscall now))
+      (boot (syscall pid 
+                     (os cur-h next-h cur-ps next-ps)))]))
+
+  (define (big-bang/os #:sound-scale sound-scale
+                       main-t)
+    (big-bang
+     (os (hasheq) (hasheq)
+         (list (process (gensym 'pid) main-t))
          empty)
-       (if (zero? frame)
-         (list (background (λ (w) se:bgm) #:gain 0.1))
-         empty))))
-   #:listener
-   (λ (w) center-pos)
-   #:done?
-   (λ (w)
-     (match-define (game-st
-                    frame serving?
-                    lhs-score rhs-score
-                    lhs-y
-                    ball-pos ball-dir ball-tar
-                    rhs-y)
-                   w)
-     (won? 4 2 lhs-score rhs-score))))
+     #:sound-scale sound-scale
+     #:tick
+     (λ (w cs)
+       (match-define (os cur-h next-h cur-ps next-ps) w)
+       (define new-w 
+         (boot
+          (os (hash-set cur-h 'controller (list (first cs)))
+              next-h cur-ps next-ps)))
+       (match-define (os new-cur-h _ _ _) new-w)
+       (values new-w
+               (gl:focus
+                width height width height
+                (psn-x center-pos) (psn-y center-pos)
+                (gl:background 0. 0. 0. 1.0
+                               (apply gl:seqn
+                                      (append
+                                       ;; XXX Figure out how to make OpenGL do this
+                                       (hash-ref new-cur-h 'graphics/first empty)
+                                       (hash-ref new-cur-h 'graphics empty)))))
+               (hash-ref new-cur-h 'sound empty)))
+     #:listener
+     (λ (w)
+       ;; XXX
+       center-pos)
+     #:done?
+     (λ (w)
+       ;; XXX
+       #f)))
+
+  ;; Use
+
+  (define (player-paddle)
+    (let loop ([lhs-y 4.5])
+      (define lhs-dy
+        (controller-dpad-y (os/read* 'controller)))
+      (define lhs-y-n
+        (clamp
+         min-paddle-y
+         (+ lhs-y (* lhs-dy speed))
+         max-paddle-y))
+      (os/write
+       (list
+        (cons 'lhs-y lhs-y-n)
+        (cons 'graphics
+              (gl:translate lhs-x (- lhs-y-n paddle-hh)
+                            lhs-paddle))))
+      (loop lhs-y-n)))
+
+  (define (ai-paddle)
+    (let loop ([rhs-y 4.5])
+      (define ball-pos
+        (os/read* 'ball-pos (start-pos 4.5 4.5 server)))
+      (define rhs-dy
+        (clamp -1.
+               (/ (- (psn-y ball-pos) rhs-y) speed)
+               1.))
+      (define rhs-y-n
+        (clamp
+         min-paddle-y
+         (+ rhs-y (* rhs-dy speed))
+         max-paddle-y))
+      (os/write
+       (list
+        (cons 'rhs-y rhs-y-n)
+        (cons 'graphics
+              (gl:translate rhs-x (- rhs-y-n paddle-hh)
+                            rhs-paddle))))
+      (loop rhs-y-n)))
+
+  (define (ball)
+    (let loop ([serving? #t]
+               [ball-pos (start-pos 4.5 4.5 server)]
+               [ball-dir (start-dir server)]
+               [ball-tar (opposite server)])
+      (define (ball-in-dir dir)
+        (+ ball-pos (make-polar ball-speed dir)))
+      (define lhs-y-n (os/read* 'lhs-y 4.5))
+      (define lhs-shape
+        (cd:aabb (psn (+ lhs-x paddle-hw) lhs-y-n)
+                 paddle-hw paddle-hh))
+      (define rhs-y-n (os/read* 'rhs-y 4.5))
+      (define rhs-shape
+        (cd:aabb (psn (+ rhs-x paddle-hw) rhs-y-n)
+                 paddle-hw paddle-hh))
+      (define rhs-serve? #t)
+      (define lhs-serve? #t)
+      (define ball-pos-m
+        (if serving?
+          (start-pos lhs-y-n rhs-y-n server)
+          (ball-in-dir ball-dir)))
+      (define ball-shape
+        (cd:aabb ball-pos-m ball-hw ball-hh))
+      (define-values
+        (ball-pos-n+ ball-dir-n ball-tar-n serving?-p sounds)
+        (cond
+          [serving?
+           (cond
+             [(and (eq? server 'right) rhs-serve?)
+              (values ball-pos-m ball-dir ball-tar #f
+                      (list (cons 'sound (sound-at se:bump-rhs ball-pos-m))))]
+             [(and (eq? server 'left) lhs-serve?)
+              (values ball-pos-m ball-dir ball-tar #f
+                      (list (cons 'sound (sound-at se:bump-lhs ball-pos-m))))]
+             [else
+              (values ball-pos-m ball-dir ball-tar serving? empty)])]
+          [; The ball hit the top
+           (cd:shape-vs-shape ball-shape frame-top)
+           (values ball-pos
+                   (case ball-tar
+                     [(left) (between 3.2 4.2)]
+                     [(right) (between 5.2 6.2)])
+                   ball-tar #f
+                   (list (cons 'sound (sound-at se:bump-wall ball-pos-m))))]
+          [; The ball hit the bot
+           (cd:shape-vs-shape ball-shape frame-bot)
+           (values ball-pos
+                   (case ball-tar
+                     [(left) (between 2.1 3.0)]
+                     [(right) (between 0.2 1.1)])
+                   ball-tar #f
+                   (list (cons 'sound (sound-at se:bump-wall ball-pos-m))))]
+          [; The ball has bounced off the lhs
+           (cd:shape-vs-shape ball-shape lhs-shape)
+           (values ball-pos
+                   (random-dir 'right) 'right #f
+                   (list (cons 'sound (sound-at se:bump-lhs ball-pos-m))))]
+          [; The ball has bounced off the rhs
+           (cd:shape-vs-shape ball-shape rhs-shape)
+           (values ball-pos
+                   (random-dir 'left) 'left #f
+                   (list (cons 'sound (sound-at se:bump-rhs ball-pos-m))))]
+          ;; The ball is inside the frame
+          [else
+           (values ball-pos-m ball-dir ball-tar #f empty)]))
+      (define ball-pos-n
+        (if (= ball-dir-n ball-dir)
+          ball-pos-n+
+          (ball-in-dir ball-dir-n)))
+      ;; XXX
+      (define lhs-score 0)
+      ;; XXX
+      (define rhs-score 0)
+      (define-values
+        (ball-pos-p ball-dir-p ball-tar-p
+                    serving?-n lhs-score-n rhs-score-n score?)
+        (cond
+          ;; The ball has moved to the left of the lhs paddle
+          [((psn-x ball-pos-n) . < . lhs-x)
+           (values (start-pos lhs-y-n rhs-y-n server) (start-dir server)
+                   (opposite server) #t lhs-score (add1 rhs-score) #t)]
+          ;; The ball has moved to the right of the rhs paddle
+          [((psn-x ball-pos-n) . > . rhs-x)
+           (values (start-pos lhs-y-n rhs-y-n server) (start-dir server)
+                   (opposite server) #t (add1 lhs-score) rhs-score #t)]
+          [else
+           (values ball-pos-n ball-dir-n ball-tar-n
+                   serving?-p lhs-score rhs-score #f)]))
+      (os/write
+       (list*
+        (cons 'ball-pos
+              ball-pos-p)
+        (cons 'graphics
+              (gl:translate (psn-x ball-pos-p) (psn-y ball-pos-p)
+                            (gl:rotate (* (/ 180 pi) ball-dir-p)
+                                       gl:ball)))
+        sounds))
+      (loop serving?-p ball-pos-p ball-dir-p ball-tar-p)))
+
+  (big-bang/os
+   #:sound-scale width-h
+   (λ ()
+     (os/thread player-paddle)
+     (os/thread ai-paddle)
+     (os/thread ball)
+     (os/write
+      (list
+       ;; XXX fix sound lambda
+       (cons 'sound (background (λ (w) se:bgm) #:gain 0.1))
+       (cons 'listener center-pos)))
+     (let loop ([lhs-score 0]
+                [rhs-score 0])
+       ;; XXX
+       (define lhs-score-n 0)
+       ;; XXX
+       (define rhs-score-n 0)
+       ;; XXX
+       (define score? #f)
+       (os/write
+        (append
+         (list
+          (cons 'done?
+                (won? 4 2 lhs-score rhs-score))
+          (cons 'graphics/first
+                (gl:seqn
+                 bgm
+                 (let ()
+                   (define score-t
+                     (gl:string->texture
+                      #:size 30
+                      (format "(~a:~a)" lhs-score-n rhs-score-n)))
+                   (gl:translate
+                    (- (psn-x center-pos) (/ (gl:texture-dw score-t) 2))
+                    (- height (gl:texture-dh score-t))
+                    (gl:seqn
+                     (gl:color 1. 1. 1. 0.
+                               (gl:rectangle (gl:texture-dw score-t)
+                                             (gl:texture-dh score-t)))
+                     (gl:color 0. 0. 0. 1.
+                               (gl:texture score-t))))))))
+         (if score?
+           (list (cons 'sound (sound-at se:applause center-pos)))
+           empty)))
+       (loop lhs-score-n
+             rhs-score-n)))))
+
+(define server 'left)
 
 (define game
   (game-info "Tennis!"
