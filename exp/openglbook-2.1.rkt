0f1632c0ae75993fa02bf997fadbd4e987057219
Jay McCarthy <jay.mccarthy@gmail.com>
2012-08-30 15:51:58 -0600

    checkpoint

--- exp/openglbook-2.1.rkt
+++ exp/openglbook-2.1.rkt
@@ -25,32 +25,31 @@
 (define VboId #f)
 (define ColorBufferId #f)
 
-(define VertexShader #<<END
-  #version 330
+(define VertexShader 
+  (string-append
+   	"#version 330\n"
 
-  layout(location=0) in vec4 in_Position;
-  layout(location=1) in vec4 in_Color;
-  out vec4 ex_Color;
+	"layout(location=0) in vec4 in_Position;\n"
+	"layout(location=1) in vec4 in_Color;\n"
+	"out vec4 ex_Color;\n"
 
-  void main(void)
-  {
-   gl_Position = in_Position;
-               ex_Color = in_Color;
-               }
-END
-  )
-(define FragmentShader #<<END
-  #version 330
+	"void main(void)\n"
+	"{\n"
+	"	gl_Position = in_Position;\n"
+	"	ex_Color = in_Color;\n"
+	"}\n"
+    ))
+(define FragmentShader 
+  (string-append
+	"#version 330\n"
 
-  in vec4 ex_Color;
-  out vec4 out_Color;
+	"in vec4 ex_Color;\n"
+	"out vec4 out_Color;\n"
 
-  void main(void)
-  {
-   out_Color = ex_Color;
-             }
-END
-  )
+	"void main(void)\n"
+	"{\n"
+	"	out_Color = ex_Color;\n"
+	"}\n"))
 
 (module+ main
   (Initialize)
@@ -59,18 +58,9 @@ END
 
 (define (Initialize)
   (InitWindow)
-  #;(set! glewExperimental #t)
-  #;(define GlewInitResult (glewInit))
-  #;(unless GlewInitResult
-    (error 'glewInit
-           (glewGetErrorString GlewInitResult)))
-
-  (printf "1\n")
 
   (printf "INFO: OpenGL Version ~a\n"
-          (glGetString GL_VERSION))
-  (printf "~v\n"
-          (list (gl-version)))
+          (glGetString GL_VERSION))  
 
   (CreateShaders)
   (CreateVBO)
@@ -108,8 +98,12 @@ END
   (set! CurrentHeight Height)
   (glViewport 0 0 CurrentWidth CurrentHeight))
 
+(define tCount 0)
 (define (RenderFunction)
   (set! FrameCount (add1 FrameCount))
+  (set! tCount (add1 tCount))
+  (when (= tCount 60)
+    (exit 1))
 
   (glClear (bitwise-ior GL_COLOR_BUFFER_BIT GL_DEPTH_BUFFER_BIT))
 
@@ -139,12 +133,36 @@ END
   (DestroyShaders)
   (DestroyVBO))
 
+(require file/sha1)
+(define (debug-f32vector v)
+  (define bs
+    (make-sized-byte-string
+     (f32vector->cpointer v)
+     (* (ctype-sizeof _float) (f32vector-length v))))
+  (debug-f32vector/bs bs)
+  v)
+(define (debug-f32vector/bs bs)  
+  (for ([pt (in-range 3)])
+    (for ([fl (in-range 4)])
+      (display
+       (bytes->hex-string
+        (subbytes bs 
+                  (+ (* pt 4 4)
+                     (* fl 4))
+                  (+ (* pt 4 4)
+                     (* fl 4)
+                     4))))
+      (printf " "))
+    (printf "\n"))
+  (printf "\n\n"))
+
 (define (CreateVBO)
   (define Vertices
     (f32vector
      -0.8 -0.8 0.0 1.0
       0.0  0.8 0.0 1.0
-     -0.8 -0.8 0.0 1.0))
+      0.8 -0.8 0.0 1.0))
+
   (define Colors
     (f32vector
      1.0 0.0 0.0 1.0
@@ -159,10 +177,11 @@ END
     (u32vector-ref (glGenBuffers 1) 0))
   (glBindBuffer GL_ARRAY_BUFFER VboId)
   (eprintf "~v ~v\n"
-           VboId (* (ctype-sizeof _float) (f32vector-length Vertices)))
+           VboId
+           (gl-vector-sizeof Vertices))
   (glBufferData GL_ARRAY_BUFFER
-                (* (ctype-sizeof _float) (f32vector-length Vertices))
-                Vertices
+                (gl-vector-sizeof Vertices)
+                (debug-f32vector Vertices)
                 GL_STATIC_DRAW)
   (glVertexAttribPointer 0 4 GL_FLOAT #f 0 0)
   (glEnableVertexAttribArray 0)
@@ -171,10 +190,11 @@ END
     (u32vector-ref (glGenBuffers 1) 0))
   (glBindBuffer GL_ARRAY_BUFFER ColorBufferId)
   (eprintf "~v ~v\n"
-           ColorBufferId (* (ctype-sizeof _float) (f32vector-length Colors)))
+           ColorBufferId
+           (gl-vector-sizeof Colors))
   (glBufferData GL_ARRAY_BUFFER
-                (* (ctype-sizeof _float) (f32vector-length Colors))
-                Colors
+                (gl-vector-sizeof Colors)
+                (debug-f32vector Colors)
                 GL_STATIC_DRAW)
   (glVertexAttribPointer 1 4 GL_FLOAT #f 0 0)
   (glEnableVertexAttribArray 1))
@@ -194,30 +214,19 @@ END
 (define (CreateShaders)
   (set! VertexShaderId (glCreateShader GL_VERTEX_SHADER))
   (glShaderSource VertexShaderId 1 (vector VertexShader)
-                  (s32vector (string-length VertexShader)))
-  (glCompileShader VertexShaderId)
-  (define-values (VinfoLen VinfoLog)
-    (glGetShaderInfoLog VertexShaderId 1024))
-  (eprintf "~a\n"
-           (subbytes VinfoLog 0 VinfoLen))
+                  (s32vector))
+  (glCompileShader VertexShaderId)  
 
   (set! FragmentShaderId (glCreateShader GL_FRAGMENT_SHADER))
   (glShaderSource FragmentShaderId 1 (vector FragmentShader)
-                  (s32vector (string-length FragmentShader)))
-  (glCompileShader FragmentShaderId)
-  (define-values (infoLen infoLog)
-    (glGetShaderInfoLog FragmentShaderId 1024))
-  (eprintf "~a\n"
-           (subbytes infoLog 0 infoLen))
+                  (s32vector))
+  (glCompileShader FragmentShaderId)  
 
   (set! ProgramId (glCreateProgram))
   (glAttachShader ProgramId VertexShaderId)
   (glAttachShader ProgramId FragmentShaderId)
   (glLinkProgram ProgramId)
-  (define-values (PinfoLen PinfoLog)
-    (glGetProgramInfoLog ProgramId 1024))
-  (eprintf "~a\n"
-           (subbytes PinfoLog 0 PinfoLen))
+  
   (glUseProgram ProgramId))
 
 (define (DestroyShaders)
@@ -226,7 +235,7 @@ END
   (glDetachShader ProgramId VertexShaderId)
   (glDetachShader ProgramId FragmentShaderId)
 
-  (glDeleteShader VertexShaderId)
   (glDeleteShader FragmentShaderId)
+  (glDeleteShader VertexShaderId)  
 
   (glDeleteProgram ProgramId))
