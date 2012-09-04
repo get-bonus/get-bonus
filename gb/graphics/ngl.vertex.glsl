18f4d5d422fa6baf3f1fb686f836fd07eb282264
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-04 00:18:25 -0600

    Revert "Trying to not use geom shader"
    This reverts commit 7ff60ec61b6a644e0af6caf57ab04866f5cae4d7.
    
    This did not work.

--- gb/graphics/ngl.vertex.glsl
+++ gb/graphics/ngl.vertex.glsl
@@ -4,43 +4,21 @@ layout(location=0) in vec4 in_Position;
 layout(location=1) in vec4 in_Color;
 layout(location=2) in int in_TexIndex;
 layout(location=3) in vec3 in_Transforms;
-layout(location=4) in int in_WhichCorner;
 
 uniform vec4 TextureAtlasIndex[~a];
-uniform ivec2 CornerMapping_Pos[4];
-uniform ivec2 CornerMapping_Tex[4];
 
-out vec4 Color;
-out vec2 TexCoord;
-
+out VertexData {
+  vec4 Color;
+  vec4 TexCoord;
+  float Rotation;
+} vertexData;
+ 
 void main(void)
 {
-  int WhichCorner = in_WhichCorner;
-
-  float x = in_Position.x;
-  float y = in_Position.y;
-  float hw = in_Position.z * in_Transforms.x;
-  float hh = in_Position.w * in_Transforms.y;
-
-  float Angle = in_Transforms.z;
-
-  mat4 RotationMatrix = mat4( cos( Angle ), -sin( Angle ), 0.0, 0.0,
-                              sin( Angle ),  cos( Angle ), 0.0, 0.0,
-                                       0.0,           0.0, 1.0, 0.0,
-                                       0.0,           0.0, 0.0, 1.0 );
-
-  gl_Position = vec4(x + CornerMapping_Pos[WhichCorner].x * hw,
-                     y + CornerMapping_Pos[WhichCorner].y * hh,
-                     0.0, 1.0) * RotationMatrix;
-  
-  Color = in_Color;
-
-  vec4 Tpos = TextureAtlasIndex[in_TexIndex];
-  float Tllx = Tpos.x;
-  float Tlly = Tpos.y;
-  float Tw = Tpos.z;
-  float Th = Tpos.w;
-
-  TexCoord = vec2(Tllx + CornerMapping_Tex[WhichCorner].x * Tw,
-                  Tlly + CornerMapping_Tex[WhichCorner].y * Th);
+  gl_Position.xy = in_Position.xy;
+  gl_Position.z = in_Position.z * in_Transforms.x;
+  gl_Position.w = in_Position.w * in_Transforms.y;
+  vertexData.Rotation = in_Transforms.z;
+  vertexData.Color = in_Color;
+  vertexData.TexCoord = TextureAtlasIndex[in_TexIndex];
 }
