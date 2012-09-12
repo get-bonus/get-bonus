75db192d78d3b5fdb1dd42b4fb9ed84514bc83d5
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-12 16:02:24 -0600

    Web sprite tool

--- tools/sprite.rkt
+++ tools/sprite.rkt
@@ -1,87 +1,77 @@
-#lang racket/gui
-(require racket/runtime-path
+#lang racket/base
+(require racket/draw
+         racket/class
+         racket/file
+         racket/match
+         racket/cmdline
+         racket/list
+         racket/path
+         file/convertible
+         racket/runtime-path
+         web-server/servlet-env
+         web-server/servlet
          gb/lib/component)
 
-(define-runtime-path resource-path "../resources")
+;; XXX Have the user select the background color
+;; XXX Have the user name the sprites, tag them, and sequence them
+;; XXX Save the sprite dictionary
+;; XXX Start from an existing sprite dictionary
+;; XXX Add keyboard shortcuts
+;; XXX Maybe put in hit shapes
 
-(module+ main
-  (define frame (new frame% [label "Sprite Sheet Tool"]))
+(define-runtime-path source-dir ".")
 
-  ;; Get the file
-  (define p
-    (parameterize ([current-directory resource-path])
-      (get-file "Please select a sprite sheet to inspect"
-                frame
-                resource-path
-                #f #f empty
-                (list (list "PNGs" "*.png")))))
+(define (xplode p)
+  (define tmp (make-temporary-file "~a.out" 'directory #f))
+
   (define bm (make-object bitmap% p 'png/alpha #f #t))
-
-  (define d
-    (new dialog% [label "Computing sprites..."]
-         [parent frame]))
-  (define m
-    (new message%
-         [label "Computing sprites..."]
-         [parent d]))
-  (send d show-without-yield)
-
-  ;; XXX Have the user select the background color
-  ;; XXX Have the user name the sprites, tag them, and sequence them
-  ;; XXX Save the sprite dictionary
-  ;; XXX Start from an existing sprite dictionary
-  ;; XXX Add keyboard shortcuts
-  ;; XXX Maybe put in hit shapes
-
-  ;; Get the sprites
   (define objs (components bm (λ (p) (zero? (pixel-a p)))))
-  (define total-sprites (vector-length objs))
-  (send d show #f)
 
-  ;; Display the sprites
-  (define vp
-    (new vertical-panel%
-         [parent frame]
-         [style '(vscroll)]
-         [min-width 800]
-         [min-height 600]
-         [stretchable-width 800]
-         [stretchable-height 600]))
-
-  (define svp
-    (new vertical-panel%
-         [parent vp]
-         [style '(vscroll)]
-         [min-width 800]
-         [min-height 600]
-         [stretchable-width 800]
-         [stretchable-height 600]))
-
-  (for ([s (in-vector objs)])
+  (for ([s (in-vector objs)]
+        [i (in-naturals)])
     (match-define (aabb x1 y1 x2 y2) s)
     (define w (- x2 x1 -1))
     (define h (- y2 y1 -1))
 
-    (define hp
-      (new horizontal-panel% [parent svp]))
-    (define c
-      (new canvas%
-           [parent hp]
-           [style '(no-autoclear)]
-           [stretchable-width w]
-           [stretchable-height h]
-           [min-width w]
-           [min-height h]))
-    (define cdc (send c get-dc))
-    (send cdc draw-bitmap-section
+    (define new-bm (make-bitmap w h))
+    (define bm-dc (new bitmap-dc% [bitmap new-bm]))
+
+    (send bm-dc draw-bitmap-section
           bm 0 0
           x1 y1 w h)
-    (new text-field%
-         [label #f]
-         [parent hp]))
 
-  (new message%
-       [label (format "Total sprites is ~a\n" total-sprites)]
-       [parent vp])
+    (send new-bm save-file (build-path tmp (format "~a.png" i)) 'png 100))
 
-  (send frame show #t))
+  (define (page/main req)
+    (response/xexpr
+     `(html
+       (head
+        (link ([rel "stylesheet"]
+               [type "text/css"]
+               [href "/sprite.css"])))
+       (body
+        (table
+         ,@(for/list ([s (in-vector objs)]
+                      [i (in-naturals)])
+             `(tr
+               (td (img ([src ,(format "~a.png" i)])))
+               (td ,(format "~a" s)))))))))
+
+  (define-values (main-dispatch main-url)
+    (dispatch-rules
+     [("sprites")
+      page/main]))
+
+  (printf "Ready\n")
+  (serve/servlet
+   main-dispatch
+   #:command-line? #t
+   #:extra-files-paths (list source-dir tmp)
+   #:servlet-path (main-url page/main)
+   #:servlet-regexp #rx""))
+
+(module+ main
+  (command-line
+   #:program "sprite-xplode"
+   #:args (file)
+   (xplode file)))
