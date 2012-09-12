28cd28a14d36545ac1e48959bfedc65359c7de6f
Jay McCarthy <jay.mccarthy@gmail.com>
2012-09-12 15:14:11 -0600

    Working on sprite tool

--- tools/sprite.rkt
+++ tools/sprite.rkt
@@ -1,88 +1,87 @@
 #lang racket/gui
 (require racket/runtime-path
-         (prefix-in gl: gb/graphics/gl)
-         gb/lib/component
-         gb/graphics/sprites)
+         gb/lib/component)
+
 (define-runtime-path resource-path "../resources")
-(current-directory resource-path)
 
-(define canvas-w 800)
-(define canvas-h 600)
+(module+ main
+  (define frame (new frame% [label "Sprite Sheet Tool"]))
 
-(define current-sprite 0)
-(define total-sprites 0)
-(define (next-sprite! b e) 
-  (set! current-sprite (modulo (add1 current-sprite) total-sprites))
-  (send progress set-value current-sprite)
-  (select-sprite!))
-(define (select-sprite!)
-  (define cdc (send c get-dc))
-  (match-define (aabb x1 y1 x2 y2) (vector-ref objs current-sprite))
-  
-  (define tw (gl:texture-w tex))
-  (define th (gl:texture-h tex))
-  (define w (- x2 x1 -1))
-  (define h (- y2 y1 -1))
-  
-  (define ratio (/ canvas-w canvas-h))
-  (define w* (max w 100))
-  (define h* (* (/ h w) w*))
-  
-  (define glctx (send cdc get-gl-context))
-  (send glctx call-as-current
-        (λ () 
-          (gl:draw 
-           w* (* h* ratio)
-           w* (* h* ratio)
-           0 0
-           (gl:background 255 0 255 0
-                          (gl:texture tex w h (/ x1 tw) (/ y1 th) (/ w tw) (/ h th))))
-          (send glctx swap-buffers)))
-  (send cdc clear))
-  
-(define frame (new frame% [label "Sprite Sheet Tool"]))
-(define c
-  (new canvas%
-       [parent frame]
-       [style '(gl no-autoclear)]
-       [min-width canvas-w]
-       [min-height canvas-h]))
-(define hp (new horizontal-pane% [parent frame]))
-(define progress
-  (new gauge% [label #f] [range 1] [parent hp]))
-(define next 
-  (new button% [parent hp] [label "Next sprite"]
-       [callback next-sprite!]))
-(send frame show #t)
+  ;; Get the file
+  (define p
+    (parameterize ([current-directory resource-path])
+      (get-file "Please select a sprite sheet to inspect"
+                frame
+                resource-path
+                #f #f empty
+                (list (list "PNGs" "*.png")))))
+  (define bm (make-object bitmap% p 'png/alpha #f #t))
 
-(define p
-  (get-file "Please select a sprite sheet to inspect"
-            frame
-            resource-path
-            #f #f empty
-            (list (list "PNGs" "*.png"))))
-(define tex (gl:path->texture p))
-(define bm (make-object bitmap% p 'png/alpha #f #t))
+  (define d
+    (new dialog% [label "Computing sprites..."]
+         [parent frame]))
+  (define m
+    (new message%
+         [label "Computing sprites..."]
+         [parent d]))
+  (send d show-without-yield)
 
-(define d 
-  (new dialog% [label "Computing sprites..."]
-       [parent frame]))
-(define m 
-  (new message% 
-       [label "Computing sprites..."]
-       [parent d]))
-(send d show-without-yield)
+  ;; XXX Have the user select the background color
+  ;; XXX Have the user name the sprites, tag them, and sequence them
+  ;; XXX Save the sprite dictionary
+  ;; XXX Start from an existing sprite dictionary
+  ;; XXX Add keyboard shortcuts
+  ;; XXX Maybe put in hit shapes
 
-; XXX Have the user select the background color
-; XXX Have the user name the sprites, tag them, and sequence them
-; XXX Save the sprite dictionary
-; XXX Start from an existing sprite dictionary
-; XXX Add keyboard shortcuts
-; XXX Maybe put in hit shapes
+  ;; Get the sprites
+  (define objs (components bm (λ (p) (zero? (pixel-a p)))))
+  (define total-sprites (vector-length objs))
+  (send d show #f)
 
-(define objs (components bm (λ (p) (zero? (pixel-a p)))))
-(set! total-sprites (vector-length objs))
-(send progress set-range total-sprites)
-(send d show #f)
+  ;; Display the sprites
+  (define vp
+    (new vertical-panel%
+         [parent frame]
+         [style '(vscroll)]
+         [min-width 800]
+         [min-height 600]
+         [stretchable-width 800]
+         [stretchable-height 600]))
 
-(select-sprite!)
+  (define svp
+    (new vertical-panel%
+         [parent vp]
+         [style '(vscroll)]
+         [min-width 800]
+         [min-height 600]
+         [stretchable-width 800]
+         [stretchable-height 600]))
+
+  (for ([s (in-vector objs)])
+    (match-define (aabb x1 y1 x2 y2) s)
+    (define w (- x2 x1 -1))
+    (define h (- y2 y1 -1))
+
+    (define hp
+      (new horizontal-panel% [parent svp]))
+    (define c
+      (new canvas%
+           [parent hp]
+           [style '(no-autoclear)]
+           [stretchable-width w]
+           [stretchable-height h]
+           [min-width w]
+           [min-height h]))
+    (define cdc (send c get-dc))
+    (send cdc draw-bitmap-section
+          bm 0 0
+          x1 y1 w h)
+    (new text-field%
+         [label #f]
+         [parent hp]))
+
+  (new message%
+       [label (format "Total sprites is ~a\n" total-sprites)]
+       [parent vp])
+
+  (send frame show #t))
