74901aa694b59ecd77e135aa37da12055056cae7
Jay McCarthy <jay.mccarthy@gmail.com>
2012-10-07 10:49:57 -0600

    evector unit chkpt

--- gb/lib/evector.rkt
+++ gb/lib/evector.rkt
@@ -1,116 +1,166 @@
 #lang racket/base
 (require racket/contract
-         racket/match)
+         racket/match
+         racket/unit)
 
-(struct evector (make-base
-                 base-ref
-                 base-set!
-                 [base #:mutable]
-                 [actual-len #:mutable]
-                 [effective-len #:mutable]))
+(define-signature vector^
+  (vector? make-vector vector-ref vector-set! vector-length))
+(define-signature evector^ extends vector^
+  (set-vector-length! vector-safe-set!))
 
-(define (make-evector make-base base-ref base-set! initial-len)
-  (evector make-base base-ref base-set!
-           (make-base initial-len)
-           initial-len
-           0))
+;; These are broken because contracts can't refer to other
+;; bidings... which is totally useless and stupid.
+;; (define-signature vector^
+;;   ((contracted
+;;     [vector?
+;;      (-> any/c boolean?)]
+;;     [make-vector
+;;      (-> exact-nonnegative-integer?
+;;          vector?)]
+;;     [vector-ref
+;;      (-> vector?
+;;          exact-nonnegative-integer?
+;;          any/c)]
+;;     [vector-set!
+;;      (-> vector?
+;;          exact-nonnegative-integer?
+;;          any/c
+;;          void)]
+;;     [vector-length
+;;      (-> vector?
+;;          exact-nonnegative-integer?)])))
+;; (define-signature evector^ extends vector^
+;;   ((contracted
+;;     [set-vector-length!
+;;      (-> vector?
+;;          exact-nonnegative-integer?
+;;          void)]
+;;     [vector-safe-set!
+;;      (-> vector?
+;;          exact-nonnegative-integer?
+;;          any/c
+;;          void)])))
 
-(define (evector-length ev)
-  (evector-effective-len ev))
-(define (set-evector-length! ev new-len)
-  (match-define (evector make-base base-ref base-set!
-                         base actual-len effective-len)
-                ev)
-  (cond
-    [(< new-len actual-len)
-     (set-evector-effective-len! ev new-len)]
-    [else
-     (define next-len (max (* 2 actual-len) new-len))
-     (define new-base (make-base next-len))
-     (for ([i (in-range effective-len)])
-       (base-set! new-base i (base-ref base i)))
-     (set-evector-base! ev new-base)
-     (set-evector-actual-len! ev next-len)
-     (set-evector-effective-len! ev new-len)]))
+(define-unit evector@
+  (import (prefix base: vector^))
+  (export evector^)
 
-(define (ensure-k ev k)
-  (unless ((evector-length ev) . > . k)
-    (error 'evector "index ~e out of bounds" k)))
+  (struct evector (base actual-len effective-len) #:mutable)
 
-(define (evector-ref ev k)
-  (ensure-k ev k)
-  ((evector-base-ref ev)
-   (evector-base ev)
-   k))
-(define (evector-set! ev k val)
-  (ensure-k ev k)
-  ((evector-base-set! ev)
-   (evector-base ev)
-   k
-   val))
-(define (evector-safe-set! ev k val)
-  (set-evector-length!
-   ev
-   (max (add1 k) (evector-length ev)))
-  (evector-set! ev k val))
+  (define vector?
+    evector?)
+
+  (define (make-vector initial-len)
+    (evector (base:make-vector initial-len)
+             initial-len
+             0))
+
+  (define (vector-length ev)
+    (evector-effective-len ev))
+  (define (set-vector-length! ev new-len)
+    (match-define (evector base actual-len effective-len)
+                  ev)
+    (cond
+      [(< new-len actual-len)
+       (set-evector-effective-len! ev new-len)]
+      [else
+       (define next-len (max (* 2 actual-len) new-len))
+       (define new-base (base:make-vector next-len))
+       (for ([i (in-range effective-len)])
+         (base:vector-set! new-base i (base:vector-ref base i)))
+       (set-evector-base! ev new-base)
+       (set-evector-actual-len! ev next-len)
+       (set-evector-effective-len! ev new-len)]))
+
+  (define (ensure-k ev k)
+    (unless ((vector-length ev) . > . k)
+      (error 'evector "index ~e out of bounds" k)))
+
+  (define (vector-ref ev k)
+    (ensure-k ev k)
+    (base:vector-ref
+     (evector-base ev)
+     k))
+  (define (vector-set! ev k val)
+    (ensure-k ev k)
+    (base:vector-set!
+     (evector-base ev)
+     k
+     val))
+  (define (vector-safe-set! ev k val)
+    (set-vector-length!
+     ev
+     (max (add1 k) (vector-length ev)))
+    (vector-set! ev k val)))
 
 (provide
- (contract-out
-  [evector?
-   (-> any/c
-       boolean?)]
-  [make-evector
-   (-> (-> exact-nonnegative-integer? any/c)
-       (-> any/c exact-nonnegative-integer? any/c)
-       (-> any/c exact-nonnegative-integer? any/c void)
-       exact-nonnegative-integer?
-       evector?)]
-  [evector-length
-   (-> evector?
-       exact-nonnegative-integer?)]
-  [set-evector-length!
-   (-> evector?
-       exact-nonnegative-integer?
-       void)]
-  [evector-ref
-   (-> evector?
-       exact-nonnegative-integer?
-       any/c)]
-  [evector-set!
-   (-> evector?
-       exact-nonnegative-integer?
-       any/c
-       void)]
-  [evector-safe-set!
-   (-> evector?
-       exact-nonnegative-integer?
-       any/c
-       void)]))
+ vector^
+ evector^
+ evector@)
+
+(define-syntax-rule
+  (define-evector prefix:
+    (prefix:vector? prefix:make-vector prefix:vector-ref
+                    prefix:vector-set! prefix:vector-length))
+  (begin
+    (define-unit prefix@
+      (import)
+      (export vector^)
+      (define vector? prefix:vector?)
+      (define make-vector prefix:make-vector )
+      (define vector-ref prefix:vector-ref)
+      (define vector-set! prefix:vector-set!)
+      (define vector-length prefix:vector-length))
+
+    (define-values/invoke-unit 
+      (compound-unit/infer 
+       (import) (export evector^)
+       (link (((PREFIX : vector^)) prefix@)
+             (() evector@ PREFIX)))
+      (import)
+      (export (prefix prefix: evector^)))))
 
 (module+ test
-  (require rackunit
-           racket/function)
+  (require rackunit)
+
+  (define-unit bytes@
+    (import)
+    (export vector^)
+    (define vector? bytes?)
+    (define make-vector make-bytes)
+    (define vector-ref bytes-ref)
+    (define vector-set! bytes-set!)
+    (define vector-length bytes-length))
+
+  (define-values/invoke-unit 
+    (compound-unit/infer 
+     (import) (export evector^)
+     (link (((BYTES : vector^)) bytes@)
+           (() evector@ BYTES)))
+    (import)
+    (export (prefix e: evector^)))
+
   (define N 100)
-  (define e (make-evector make-bytes bytes-ref bytes-set! 10))
+  (define e (e:make-vector 10))
   (for* ([try (in-range 2)]
          [i (in-range N)])
-    (set-evector-length! e (add1 i))
-    (check-equal? (evector-length e) (add1 i)
+    (e:set-vector-length! e (add1 i))
+    (check-equal? (e:vector-length e) (add1 i)
                   (format "~a len" i))
-    (evector-set! e i i)
+    (e:vector-set! e i i)
     (for ([j (in-range N)])
       (if (j . <= . i)
-        (check-equal? (evector-ref e j) j
+        (check-equal? (e:vector-ref e j) j
                       (format "~a ~a valid" i j))
         (check-exn exn:fail?
-                   (λ () (evector-ref  e j))
+                   (λ () (e:vector-ref  e j))
                    (format "~a ~a invalid" i j)))))
-  (define ep (make-evector make-bytes bytes-ref bytes-set! 10))
+  (define ep (e:make-vector 10))
   (define order (build-list N (λ (i) (random N))))
   (for ([i (in-list order)]
         [which (in-naturals)])
-    (evector-safe-set! ep i i)
+    (e:vector-safe-set! ep i i)
     (for ([j (in-list order)]
           [_ (in-range which)])
-      (check-equal? (evector-ref ep j) j
+      (check-equal? (e:vector-ref ep j) j
                     (format "~a ~a valid" i j)))))
