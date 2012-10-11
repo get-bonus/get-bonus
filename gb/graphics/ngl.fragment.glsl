02987b2075a9e00fee16f291fc2d43a1b74e4352
Jay McCarthy <jay.mccarthy@gmail.com>
2012-10-10 19:31:06 -0600

    notes

--- gb/graphics/ngl.fragment.glsl
+++ gb/graphics/ngl.fragment.glsl
@@ -9,6 +9,8 @@ out vec4 out_Color;
  
 void main(void)
 {
+  // XXX Would it be faster to use texelFetch with integer coords?
+
   // XXX Do proper blending, allow color to set the alpha, etcs
   vec2 TexCoord_uv = vec2(floor(TexCoord.x)+0.5, floor(TexCoord.y)+0.5) / TextureAtlasSize;
   out_Color = Color + texture2D(TextureAtlas, TexCoord_uv);
