9092470f8b09052342647720f1a5d052fe0e54f4
Jay McCarthy <jay.mccarthy@gmail.com>
2012-10-20 14:03:49 -0600

    notes

--- gb/graphics/ngl.fragment.glsl
+++ gb/graphics/ngl.fragment.glsl
@@ -11,8 +11,14 @@ void main(void)
 {
   // XXX Would it be faster to use texelFetch with integer coords?
 
-  // XXX Do proper blending, allow color to set the alpha, etcs
+  // I changed from floor to ceil to fix the menu, but I'm worried
+  // that I will eventually find some that need to be balanced on the
+  // other side. Maybe I should preserve the original pixel boundaries
+  // coordinates better and round towards them?
   vec2 TexCoord_uv = vec2(ceil(TexCoord.x)+0.5, ceil(TexCoord.y)+0.5) / TextureAtlasSize;
+  // Blurry version:
   // vec2 TexCoord_uv = TexCoord / TextureAtlasSize;
+
+  // XXX Do proper blending, allow color to set the alpha, etcs
   out_Color = Color + texture2D(TextureAtlas, TexCoord_uv);
 }
