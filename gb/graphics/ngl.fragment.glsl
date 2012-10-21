8f3a7ab8745f33e4ee79d3a6fa1cd29172d56ac6
Jay McCarthy <jay.mccarthy@gmail.com>
2012-10-20 21:35:48 -0600

    adjusting fragment shader

--- gb/graphics/ngl.fragment.glsl
+++ gb/graphics/ngl.fragment.glsl
@@ -6,18 +6,37 @@ uniform int TextureAtlasSize;
 in vec4 Color;
 in vec2 TexCoord;
 out vec4 out_Color;
+
+float blurry_mess ( float v ) {
+  return v;
+}
+
+float works_for_menu ( float v ) {
+  return ceil(v)+0.5;
+}
+
+float works_for_tennis_bg ( float v ) {
+  return round(v)+0.5;
+}
+
+// XXX This is strange, because before I changed menu's resolution,
+// floor was broken on it.
+float works_for_tennis_bg_and_menu ( float v ) {
+  return floor(v)+0.5;
+}
+
+float clampit ( float v ) {
+  //return works_for_menu(v);
+  return works_for_tennis_bg_and_menu(v);
+}
  
 void main(void)
 {
   // XXX Would it be faster to use texelFetch with integer coords?
 
-  // I changed from floor to ceil to fix the menu, but I'm worried
-  // that I will eventually find some that need to be balanced on the
-  // other side. Maybe I should preserve the original pixel boundaries
-  // coordinates better and round towards them?
-  vec2 TexCoord_uv = vec2(ceil(TexCoord.x)+0.5, ceil(TexCoord.y)+0.5) / TextureAtlasSize;
-  // Blurry version:
-  // vec2 TexCoord_uv = TexCoord / TextureAtlasSize;
+  vec2 TexCoord_uv = 
+    vec2(clampit(TexCoord.x), clampit(TexCoord.y))
+    / TextureAtlasSize;
 
   // XXX Do proper blending, allow color to set the alpha, etcs
   out_Color = Color + texture2D(TextureAtlas, TexCoord_uv);
