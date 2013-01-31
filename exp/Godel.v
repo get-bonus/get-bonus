2110253efee8c1335a85964e9455b5ce6f61e79c
Jay McCarthy <jay.mccarthy@gmail.com>
2013-01-31 15:23:18 -0700

    Checkpoint

--- exp/Godel.v
+++ exp/Godel.v
@@ -10,9 +10,7 @@ Inductive ni_lt : nat -> nati -> Prop :=
 | ni_lt_n : forall n m, n < m -> ni_lt n (a_nat m).
 Hint Constructors ni_lt.
 
-Class FIso (X:Type) := {
- k : nati ;
- k_nz : ni_lt 0 k ;
+Class FIso (X:Type) (k : nati) := {
  i : nat -> option X ;
  i_k : forall n, ni_lt n k -> exists x, i n = Some x ;
  o : X -> option nat ;
@@ -22,22 +20,19 @@ Class FIso (X:Type) := {
  io : forall x n, (o x) = Some n -> forall x', (i n) = Some x' -> eql x x'
 }.
 
-Instance nat_FIso : FIso nat:= {
- k := pos_inf ;
+Instance nat_FIso : FIso nat pos_inf := {
  i := fun n => Some n ;
  o := fun x => Some x ;
  eql := eq
 }.
 Proof.
- auto. 
  intros n n_LT. exists n. auto.
  auto.
  intros n n_LT x EQ. inversion_clear EQ. auto.
  intros x n EQ. inversion_clear EQ. intros x' EQ. inversion_clear EQ. auto.
 Qed.
 
-Instance bool_FIso : FIso bool := {
- k := a_nat 2 ;
+Instance bool_FIso : FIso bool (a_nat 2) := {
  i := fun n => 
   match n with 
   | 0 => Some false
@@ -48,8 +43,6 @@ Instance bool_FIso : FIso bool := {
  eql := eq
 }.
 Proof.
- auto.
-
  intros [|[|]]; eauto.
  intros n n_LT. inversion_clear n_LT. inversion_clear H. inversion_clear H0. inversion_clear H.
 
@@ -60,58 +53,56 @@ Proof.
  intros [|] n EQ; inversion_clear EQ; intros x' EQ; inversion_clear EQ; auto.
 Qed.
 
-Inductive fnat (k:nat) : Set :=
-| fnat_n : forall n, n < k -> fnat k.
-Hint Constructors fnat. 
-
-Inductive fnat_eq (k:nat) : (fnat k) -> (fnat k) -> Prop :=
-| fnat_eq_refl : forall n p m p', n = m -> fnat_eq k (fnat_n k n p) (fnat_n k m p').
-Hint Constructors fnat_eq. 
-
-Instance fnat_FIso (fk:nat) (fk_z:fk > 0) : FIso (fnat fk) := {
- k := a_nat fk ;
+Instance fnat_FIso (fk:nat) : FIso nat (a_nat fk) := {
  i := fun n => match lt_dec n fk with 
-               | left n_LT => fnat_n fk n n_LT
-               | right _ => fnat_n fk 0 _
+               | left _ => Some n
+               | right _ => None
                end ;
- o := fun x => match x with fnat_n n _ => n end ;
- eql := fnat_eq fk
+ o := fun n => match lt_dec n fk with 
+               | left _ => Some n
+               | right _ => None
+               end ;
+ eql := eq
 }.
 Proof.
- auto.
-
- intros [n n_LT]. auto.
-
- intros n n_iLT. destruct (lt_dec n fk) as [n_LT | n_NLT].
+ intros n n_iLT. exists n. destruct (lt_dec n fk) as [n_LT | n_NLT].
  reflexivity. absurd (n < fk). auto. inversion n_iLT. auto.
 
- intros [n n_LT]. destruct (lt_dec n fk) as [n_LT' | n_NLT]; auto.
- absurd (n < fk); auto.
+ intros x n. destruct (lt_dec x fk) as [x_LT | x_NLT]; intro EQ; try inversion EQ.
+ rewrite <- H0. auto.
 
- Grab Existential Variables.
- auto.
+ intros n n_iLT x. remember (lt_dec n fk) as p.
+ destruct p as [n_LT | n_NLT]; intros EQ; try inversion EQ.
+ rewrite <- H0. rewrite <- Heqp. auto.
+
+ intros x n. remember (lt_dec x fk) as p.
+ destruct p as [x_LT | x_NLT]; intros EQ; try inversion EQ.
+ intros x'. 
+ rewrite <- H0. rewrite <- Heqp. auto.
+ intros EQ'; inversion EQ'. auto.
 Qed.
 
 (* XXX We need "oddbits", "evenbits" and "combinebits" to do this *)
-Instance nat_nat_FIso : FIso (nat*nat) := {
- k := pos_inf
+Instance inat_inat_FIso 
+          (x:FIso nat pos_inf)
+          (y:FIso nat pos_inf)
+ : FIso (nat*nat) pos_inf := {
 }.
 Admitted.
 
 (* XXX This is based on Z = X + Y * XK *)
 Instance fnat_fnat_FIso 
-          (xk:nat) (x:FIso (fnat xk))
-          (yk:nat) (x:FIso (fnat yk)) 
-         : FIso ((fnat xk) * (fnat yk)) := {
- k := a_nat (xk * yk)
+          (xk:nat) (x:FIso nat (a_nat xk))
+          (yk:nat) (y:FIso nat (a_nat yk)) 
+         : FIso (nat*nat) (a_nat (xk * yk)) := {
 }.
 Admitted.
 
 (* XXX This is based on Z = X + Y * XK *)
 Instance fnat_nat_FIso 
-          (xk:nat) (x:FIso (fnat xk)) 
-         : FIso ((fnat xk) * nat) := {
- k := pos_inf
+          (xk:nat) (x:FIso nat (a_nat xk))
+          (y:FIso nat pos_inf) 
+         : FIso (nat*nat) pos_inf := {
 }.
 Admitted.
 
@@ -120,45 +111,65 @@ Inductive flip_eql (X Y:Type) (eql:(Y*X) -> (Y*X) -> Prop) : (X*Y) -> (X*Y) -> P
 Hint Constructors flip_eql.
 
 Instance flip_FIso 
-           (X Y:Type) (YX_FIso:FIso (Y*X))
-         : FIso (X*Y) := {
- k := k ;
- i := fun n => let (y,x) := (i n : (Y*X)) in (x,y) ;
- o := fun p => let (x,y) := p in (o (y,x)) ;
+           (X Y:Type) (k:nati) (YX_FIso:FIso (Y*X) k)
+         : FIso (X*Y) k := {
+ i := fun n => 
+      match i n with
+      | Some (y, x) =>
+        Some (x, y)
+      | None =>
+        None
+      end ;
+ o := fun p => 
+      let (x,y) := p in 
+       (o (y,x)) ;
  eql := flip_eql X Y eql
 }.
-Proof.
- apply k_nz.
- 
+Proof. 
+ intros n n_LT. destruct (i_k n n_LT) as [[y x] EQ].
+ exists (x,y). rewrite EQ. auto.
+
  intros [x y]; simpl. apply o_k.
 
- intros n n_LT. remember (i n) as p. destruct p as [y x].
- rewrite Heqp. apply oi. auto.
+ intros n n_LT. remember (i n) as p. destruct p as [[y x]|];
+  intros p EQ; inversion_clear EQ. apply oi; auto.
 
- intros [x y]; simpl. remember (i (o (y, x))) as p. destruct p as [y' x'].
- apply flip_eql_inner. rewrite Heqp. apply io.
+ intros [x y] n EQ [x' y']; simpl. remember (i n) as p. destruct p as [[y'' x'']|];
+  intros EQ'; inversion EQ'.
+ apply flip_eql_inner. eapply io. apply EQ.
+ rewrite <- Heqp. inversion EQ'. auto.
 Qed.
 
 Instance nat_fnat_FIso 
-          (yk:nat) (y:FIso (fnat yk)) 
-         : FIso (nat * (fnat yk)).
+          (x:FIso nat pos_inf) 
+          (yk:nat) (y:FIso nat (a_nat yk)) 
+         : FIso (nat * nat) pos_inf.
 Proof.
  apply flip_FIso.
- apply fnat_nat_FIso.
- auto.
+ eapply fnat_nat_FIso; auto.
+ apply y.
 Qed.
 
-Instance unit_FIso (X:Type) (x:X) : FIso X := {
- k := a_nat 1 ;
- i := fun n => x ;
- o := fun x => 0 ;
+Instance unit_FIso (X:Type) (x:X) (is_x_dec : (forall x', { x' = x } + { x' <> x }))
+ : FIso X (a_nat 1) := {
+ i := fun n => match n with 0 => Some x | _ => None end ;
+ o := fun x' => if is_x_dec x' then Some 0 else None ;
  eql := eq
 }.
 Proof.
- auto.
+ intros n n_LT. exists x.
+ destruct n as [|n]; auto.
+ inversion n_LT. inversion H1. inversion H3.
 
- auto.
- 
- intros n n_LT. inversion n_LT. inversion H1. reflexivity. inversion H3.
+ intros x' n. destruct (is_x_dec x');
+  intros EQ; inversion EQ. auto.
 
- reflexivity.
+ intros n n_LT x'.
+  destruct n as [|n]; intros EQ; symmetry in EQ; inversion_clear EQ.
+  destruct (is_x_dec x); auto.
+  absurd (x = x); auto.
+
+ intros x' n. destruct (is_x_dec x'); intros EQ; inversion_clear EQ.
+ intros x'' EQ. symmetry in EQ. inversion_clear EQ. auto.
+Qed.
+
