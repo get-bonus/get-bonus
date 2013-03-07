11d63153698282a4ecfc04a97a9da24c1bef2683
Jay McCarthy <jay.mccarthy@gmail.com>
2013-03-07 14:08:30 -0700

    pi

--- exp/pi.rkt
+++ exp/pi.rkt
@@ -1,69 +1,32 @@
-#lang typed/racket/base
-(require math/number-theory
-         math/base)
+#lang racket/base
+(require racket/generator)
 
 (module+ main
-  (define-values (_a1)
-    (for/fold:
-     ([8Pi : Integer -8])
-     ([i : Integer (in-range 50)])
+  (define (digits)
+    (let loop ([8Pi -8])
+      (define 8i
+        (+ 8 8Pi))
 
-     (define 8i 
-       (+ 8 8Pi))
+      (define (E k)
+        (/ 1 (+ 8i k)))
 
-     (: E (Integer -> Real))
-     (define (E k)
-       (/ 1 (+ 8i k)))
-     
-     (define pi_i
-       (+ (* +4 (E 1))
-          (* -2 (E 4))
-          (* -1 (E 5))
-          (* -1 (E 6))))
+      (define pi_i
+        (+ (* +4 (E 1))
+           (* -2 (E 4))
+           (* -1 (E 5))
+           (* -1 (E 6))))
 
-     (printf "~s " (number->string pi_i))
+      (for ([c (in-string (number->string pi_i))])
+        (unless (eq? #\/ c)
+          (yield (string->number (string c)))))
 
-     (values 8i)))
-  (newline)
-  (exit 1)
+      (loop 8i)))
 
-  (define-values (_a _b _c _d)
-    (for/fold:
-     ([8Pi : Integer -8]
-      [16^Pi : Real 1/16]
-      [10^Pi : Real 1/10]
-      [pi_Pi : Real 0])
-     ([i : Integer (in-range 500)])
+  (define d->i (make-hasheq))
+  (for ([i (in-range 5000)]
+        [d (in-generator (digits))])
+    (hash-update! d->i d add1 0))
 
-     (define 8i 
-       (+ 8 8Pi))
-
-     (: E (Integer -> Real))
-     (define (E k)
-       (/ 1 (+ 8i k)))
-
-     (define 16^i 
-       (* 16^Pi 16))
-     (define pi_i_minus_pi_Pi
-       (/ (+ (* +4 (E 1))
-             (* -2 (E 4))
-             (* -1 (E 5))
-             (* -1 (E 6)))
-          16^i))
-     (define pi_i
-       (+ pi_Pi pi_i_minus_pi_Pi))
-
-     (define 10^i
-       (* 10 10^Pi))
-
-     (printf "~a"
-             (remainder
-              (exact-truncate
-               (* pi_i 10^i))
-              10))
-
-     (values 8i
-             16^i
-             10^i
-             pi_i)))
+  (for ([i (in-range 10)])
+    (printf "~a => ~a\n" i (hash-ref d->i i)))
   (newline))
