dadb46d11b20585500483b21bb6ab9bb5b513cf6
Jay McCarthy <jay.mccarthy@gmail.com>
2014-12-20 21:47:05 -0500

    work around opengl problem

--- gb/graphics/ngl.rkt
+++ gb/graphics/ngl.rkt
@@ -24,13 +24,63 @@
          gb/lib/performance-log
          gb/lib/math
          gb/lib/gzip
-         opengl)
+         (except-in opengl
+                    bitmap->texture
+                    load-texture))
 
 (define debug? #f)
 
 (define sprite-tree/c
   ;; XXX really a tree of sprite-info?, but that's expensive to check
   any/c)
+
+;; COPIED FROM opengl/main
+;; Convert argb -> rgba, and convert to pre-multiplied alpha.
+;; (Non-premultiplied alpha gives blending artifacts and is evil.)
+;; Modern wisdom is not to convert to rgba but rather use 
+;; GL_BGRA with GL_UNSIGNED_INT_8_8_8_8_REV. But that turns out not
+;; to work on some implementations, even ones which advertise
+;; OpenGL 1.2 support. Great.
+(define (argb->rgba! pixels)
+  (for ((i (in-range (/ (bytes-length pixels) 4))))
+       (let* ((offset (* 4 i))
+              (alpha (bytes-ref pixels offset))
+              (red (bytes-ref pixels (+ 1 offset)))
+              (green (bytes-ref pixels (+ 2 offset)))
+              (blue (bytes-ref pixels (+ 3 offset))))
+         (bytes-set! pixels offset (quotient (* alpha red) 255))
+         (bytes-set! pixels (+ 1 offset) (quotient (* alpha green) 255))
+         (bytes-set! pixels (+ 2 offset) (quotient (* alpha blue) 255))
+         (bytes-set! pixels (+ 3 offset) alpha))))
+
+(define (bitmap->texture bm)
+  (local-require racket/class)
+  (let* ((w (send bm get-width))
+         (h (send bm get-height))
+         (pixels (make-bytes (* w h 4)))
+         (texture (u32vector-ref (glGenTextures 1) 0)))
+
+    (define (load-texture-data)
+      (glTexImage2D GL_TEXTURE_2D 0 GL_RGBA8 w h 0 GL_RGBA GL_UNSIGNED_BYTE pixels))
+
+    (send bm get-argb-pixels 0 0 w h pixels)
+    ;; massage data.
+    (argb->rgba! pixels)
+    
+    (glBindTexture GL_TEXTURE_2D texture)
+    (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_S GL_CLAMP_TO_EDGE)
+    (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_WRAP_T GL_CLAMP_TO_EDGE)
+    (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MAG_FILTER GL_LINEAR)
+    (glTexParameteri GL_TEXTURE_2D GL_TEXTURE_MIN_FILTER GL_LINEAR)
+    (load-texture-data)
+
+    texture))
+
+;; Directly load a file from disk as texture.
+(define (load-texture filename)
+  (local-require racket/gui/base)
+  (bitmap->texture (read-bitmap filename)))
+;; </COPIED>
 
 (provide
  (contract-out
@@ -68,7 +118,7 @@
    ;; total.)
    [pal _uint16]   ;; 11
    [spr _uint16]   ;; 12
-
+   
    [horiz _sint8]  ;; 13
    [vert _sint8])) ;; 14
 
@@ -221,12 +271,8 @@
                   GL_RED GL_UNSIGNED_BYTE
                   sprite-atlas-bytes))
 
-  ;; XXX load-texture is broken in Core because it uses GL_CLAMP
-  ;; rather than TO_EDGE or TO_BORDER. I hacked around it but should
-  ;; be fixed better.
   (define PaletteAtlasId
-    (load-texture palette-atlas-path
-                  #:mipmap #f))
+    (load-texture palette-atlas-path))
 
   (define SpriteIndexId (u32vector-ref (glGenTextures 1) 0))
   (glBindTexture GL_TEXTURE_2D SpriteIndexId)
